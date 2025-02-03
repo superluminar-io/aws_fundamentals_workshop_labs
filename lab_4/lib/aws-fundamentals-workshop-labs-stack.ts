341a0e2ae833244ddd5ae67bc55044a4136f01b7
Nora Schöner <nora.schoener@superluminar.io>
2025-02-03 21:25:21 +0100

    update code for lab 4 & 5 using go container

--- lab_4/lib/aws-fundamentals-workshop-labs-stack.ts
+++ lab_4/lib/aws-fundamentals-workshop-labs-stack.ts
@@ -49,12 +49,17 @@ export class AwsFundamentalsWorkshopLabsStack extends Stack {
         // Create a Fargate Task Definition with a Container
         const fargateTaskDefinition = new FargateTaskDefinition(this, 'TaskDef');
         fargateTaskDefinition.addContainer('AppContainer', {
-            containerName: 'web',
-            image: ContainerImage.fromRegistry('nginx:latest'),
-            memoryLimitMiB: 512,
-            cpu: 256,
-            logging: LogDrivers.awsLogs({streamPrefix: 'myApp/nginx'}),
-            portMappings: [{containerPort: 80}],
+          containerName: 'web',
+          image: ContainerImage.fromRegistry('ghcr.io/superluminar-io/dct:latest'),
+          memoryLimitMiB: 512,
+          cpu: 256,
+          logging: LogDrivers.awsLogs({streamPrefix: 'myApp/webapp'}),
+          portMappings: [{containerPort: 8081}],
+          environment: {
+            DB_HOST: 'some-host',
+            DB_USERNAME: 'some-user',
+            DB_PASSWORD: 'some-password',
+          }
         });
 
         // Create a Fargate Service
@@ -73,8 +78,8 @@ export class AwsFundamentalsWorkshopLabsStack extends Stack {
         service.registerLoadBalancerTargets(
             {
                 containerName: 'web',
-                containerPort: 80,
-                newTargetGroupId: 'ecs_nginx',
+                containerPort: 8081,
+                newTargetGroupId: 'ecs_webapp',
                 listener: ListenerConfig.applicationListener(listener, {
                     protocol: ApplicationProtocol.HTTP,
                 }),
--- lab_5/lib/aws-fundamentals-workshop-labs-stack.ts
+++ lab_5/lib/aws-fundamentals-workshop-labs-stack.ts
@@ -1,38 +1,30 @@
-import {
-  CfnOutput,
-  Duration,
-  RemovalPolicy,
-  Stack,
-  StackProps,
-} from 'aws-cdk-lib'
+import {aws_secretsmanager, CfnOutput, Duration, RemovalPolicy, Stack, StackProps} from 'aws-cdk-lib'
 import {
   SubnetType,
   Vpc,
-  SecurityGroup,
-  Peer,
-  Port,
-  Instance,
-  InstanceType,
-  InstanceClass,
-  InstanceSize,
-  MachineImage,
-  UserData,
+  SecurityGroup, InstanceType, InstanceClass, InstanceSize,
 } from 'aws-cdk-lib/aws-ec2'
 import {
   ArnPrincipal,
-  ManagedPolicy,
   PolicyStatement,
-  Role,
-  ServicePrincipal,
 } from 'aws-cdk-lib/aws-iam'
+import {BlockPublicAccess, Bucket} from 'aws-cdk-lib/aws-s3'
+import {Construct} from 'constructs'
+import {
+  Cluster,
+  ContainerImage,
+  FargateService,
+  FargateTaskDefinition,
+  ListenerConfig,
+  LogDrivers, Secret
+} from "aws-cdk-lib/aws-ecs";
+import {ApplicationLoadBalancer, ApplicationProtocol} from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import {
   Credentials,
   DatabaseInstance,
   DatabaseInstanceEngine,
-  MysqlEngineVersion,
-} from 'aws-cdk-lib/aws-rds'
-import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3'
-import { Construct } from 'constructs'
+  MysqlEngineVersion
+} from "aws-cdk-lib/aws-rds";
 
 export class AwsFundamentalsWorkshopLabsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -55,19 +47,6 @@ export class AwsFundamentalsWorkshopLabsStack extends Stack {
       ],
     })
 
-    // Security Group for EC2 instance
-    const ec2SecurityGroup = new SecurityGroup(this, 'EC2SecurityGroup', {
-      vpc,
-      allowAllOutbound: true,
-      description: 'Allow HTTP access to EC2 instance',
-    })
-
-    // Allow HTTP access to the EC2 instance
-    ec2SecurityGroup.addIngressRule(
-      Peer.anyIpv4(),
-      Port.tcp(80),
-      'Allow HTTP access'
-    )
 
     // Security Group for RDS instance
     const rdsSecurityGroup = new SecurityGroup(this, 'RDSSecurityGroup', {
@@ -75,67 +54,14 @@ export class AwsFundamentalsWorkshopLabsStack extends Stack {
       allowAllOutbound: true,
       description: 'Allow MySQL access to RDS instance',
     })
-    rdsSecurityGroup.addIngressRule(
-      ec2SecurityGroup,
-      Port.tcp(3306),
-      'Allow MySQL access from EC2 instance'
-    )
 
-    // IAM role for EC2 instance to use SSM
-    const role = new Role(this, 'SSMRole', {
-      assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
-    })
-
-    // Attach the AmazonSSMManagedInstanceCore managed policy to the role
-    role.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
-    )
-
-    // Add S3 read permissions to the EC2 instance role
-    role.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess')
-    )
-
-    // Create an EC2 instance
-    const ec2Instance = new Instance(this, 'MyEC2Instance', {
-      vpc,
-      instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
-      machineImage: MachineImage.latestAmazonLinux2(),
-      securityGroup: ec2SecurityGroup,
-      vpcSubnets: { subnetType: SubnetType.PUBLIC },
-      role: role,
-      userData: UserData.forLinux(),
-    })
-
-    // Install AWS CLI on the EC2 instance
-    ec2Instance.addUserData(
-      'yum update -y',
-      'yum install -y aws-cli',
-      'echo "AWS CLI installed. You can now use AWS S3 commands to test bucket access."'
-    )
-
-    // Create an S3 bucket
-    const bucket = new Bucket(this, 'MyBucket', {
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      publicReadAccess: false, // Ensure the bucket is not publicly accessible
-      blockPublicAccess: BlockPublicAccess.BLOCK_ALL, // Block all public access
-    })
-
-    // Add a bucket policy that allows access from the EC2 instance
-    bucket.addToResourcePolicy(
-      new PolicyStatement({
-        actions: [
-          's3:GetObject',
-          's3:ListBucket',
-          's3:PutObject',
-          's3:DeleteObject',
-          's3:DeleteBucket',
-        ],
-        resources: [bucket.bucketArn, bucket.arnForObjects('*')],
-        principals: [new ArnPrincipal(ec2Instance.role.roleArn)],
-      })
-    )
+    // Create a secret for the RDS instance
+    const databaseCredentials = Credentials.fromGeneratedSecret('admin',
+      {
+        secretName: 'MyRDSSecret'
+      }
+    );
+    const databaseSecret = aws_secretsmanager.Secret.fromSecretNameV2(this, 'MyRDSSecret', databaseCredentials.secretName!);
 
     // Create an RDS instance
     const rdsInstance = new DatabaseInstance(this, 'MyRDSInstance', {
@@ -152,7 +78,7 @@ export class AwsFundamentalsWorkshopLabsStack extends Stack {
       // select the security group we created
       securityGroups: [rdsSecurityGroup],
       // set the credentials to be generated in AWS Secrets Manager
-      credentials: Credentials.fromGeneratedSecret('admin'), // Generates a secret in Secrets Manager
+      credentials: databaseCredentials, // Generates a secret in Secrets Manager
       // set the multi-az to false for a single-az deployment
       multiAz: false,
       // select the allocated storage
@@ -171,6 +97,79 @@ export class AwsFundamentalsWorkshopLabsStack extends Stack {
       databaseName: 'MyDatabase',
     })
 
+
+    // Create the ECS Cluster
+    const cluster = new Cluster(this, 'FargateCluster', {
+      vpc,
+    });
+
+    // Create a Fargate Task Definition with a Container
+    const fargateTaskDefinition = new FargateTaskDefinition(this, 'TaskDef');
+    fargateTaskDefinition.addContainer('AppContainer', {
+      containerName: 'web',
+      image: ContainerImage.fromRegistry('ghcr.io/superluminar-io/dct:latest'),
+      memoryLimitMiB: 512,
+      cpu: 256,
+      logging: LogDrivers.awsLogs({streamPrefix: 'myApp/webapp'}),
+      portMappings: [{containerPort: 8081}],
+      secrets: {
+        DB_HOST: Secret.fromSecretsManager(databaseSecret, 'host'),
+        DB_USERNAME: Secret.fromSecretsManager(databaseSecret, 'username'),
+        DB_PASSWORD: Secret.fromSecretsManager(databaseSecret, 'password'),
+      }
+    });
+
+
+    // Create a Fargate Service
+    const service = new FargateService(this, 'FargateService', {
+      cluster,
+      taskDefinition: fargateTaskDefinition,
+      minHealthyPercent: 100,
+      vpcSubnets: {subnetType: SubnetType.PRIVATE_WITH_EGRESS},
+      enableExecuteCommand: true,
+    });
+
+    // Create an Application Load Balancer that listens on port 80
+    const lb = new ApplicationLoadBalancer(this, 'LoadBalancer', {vpc, internetFacing: true});
+    const listener = lb.addListener('LBListener', {port: 80});
+
+    // Register the ECS Service as a target of the Application Load Balancer
+    service.registerLoadBalancerTargets(
+      {
+        containerName: 'web',
+        containerPort: 8081,
+        newTargetGroupId: 'ecs_webapp',
+        listener: ListenerConfig.applicationListener(listener, {
+          protocol: ApplicationProtocol.HTTP,
+        }),
+      },
+    );
+
+    rdsInstance.connections.allowDefaultPortFrom(service, 'Allow access from ECS service')
+
+    // Create an S3 bucket
+    const bucket = new Bucket(this, 'MyBucket', {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: false, // Ensure the bucket is not publicly accessible
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL, // Block all public access
+    })
+
+    // Add a bucket policy that allows access from the ECS service
+    bucket.addToResourcePolicy(
+      new PolicyStatement({
+        actions: [
+          's3:GetObject',
+          's3:ListBucket',
+          's3:PutObject',
+          's3:DeleteObject',
+          's3:DeleteBucket',
+        ],
+        resources: [bucket.bucketArn, bucket.arnForObjects('*')],
+        principals: [new ArnPrincipal(service.taskDefinition.taskRole.roleArn)],
+      })
+    )
+
     // Output the RDS instance endpoint
     new CfnOutput(this, 'RDSInstanceEndpoint', {
       value: rdsInstance.dbInstanceEndpointAddress,
@@ -186,21 +185,18 @@ export class AwsFundamentalsWorkshopLabsStack extends Stack {
       value: rdsInstance.secret?.secretArn || '',
     })
 
+    //Output the Load Balancer DNS Name for easy reference
+    new CfnOutput(this, 'LoadBalancerDNS', {
+      value: lb.loadBalancerDnsName,
+      description: 'DNS Name of the Application Load Balancer',
+    })
+
     // Output the bucket name for easy reference
     new CfnOutput(this, 'BucketName', {
       value: bucket.bucketName,
       description: 'Name of the S3 bucket',
     })
 
-    // Output the EC2 instance ID
-    new CfnOutput(this, 'EC2InstanceId', {
-      value: ec2Instance.instanceId,
-    })
-
-    // Output the Security Group IDs
-    new CfnOutput(this, 'EC2SecurityGroupId', {
-      value: ec2SecurityGroup.securityGroupId,
-    })
     new CfnOutput(this, 'RDSSecurityGroupId', {
       value: rdsSecurityGroup.securityGroupId,
     })
